627006ac459ef05f62956e2074ab0545ab0d0683
Jusna Begum <jb@juweez.co.uk>
2017-06-05 21:57:14 +0100

    Create separate production and dev Webpack configs

---
+++ webpack.base.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|build\/)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets:['es2015','react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: []
+};
---
+++ webpack.dev.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const config = require('./webpack.base.config');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+
+config.output = {
+  publicPath: './',
+  path: path.join(__dirname, 'build'),
+  filename: 'bundle.js'
+};
+
+config.module.rules.push({
+  test: /\.css$/,
+  use: [
+    { loader: 'style-loader' },
+    {
+      loader: 'css-loader',
+      options: {
+        importLoaders: 1,
+        modules: true,
+        localIdentName: '[name]__[local]___[hash:base64:5]'
+      }
+    }
+  ]
+});
+
+config.devServer = {
+  contentBase: "./build"
+};
+
+config.plugins.push(new HtmlWebpackPlugin({
+  template: './src/template.html',
+}));
+
+config.plugins.push(new DashboardPlugin());
+
+module.exports = config;
