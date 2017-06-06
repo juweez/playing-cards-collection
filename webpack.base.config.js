23e06274aa96c3ce8d7964d866a6598ac598086c
Jusna Begum <jb@juweez.co.uk>
2017-06-06 22:20:33 +0100

    test 3

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -15,7 +15,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           query: {
-            presets:['es2015','react']
+            presets:['es2015','react'],
           }
         }
       }
