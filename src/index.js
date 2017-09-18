4b128a040eb45c4306c92924c13ca629b5cf69cc
Jusna Begum <jb@juweez.co.uk>
2017-09-18 21:11:34 +0100

    Sort cards alphabetically

--- src/index.js
+++ src/index.js
@@ -46,7 +46,12 @@ class App extends Component {
   }
 
   render() {
-    const cards = this.state.cards.map(item => (
+    const sortedCards = this.state.cards.sort(function(a, b){
+      if(a.title < b.title) return -1;
+      if(a.title > b.title) return 1;
+      return 0;
+    });
+    const cards = sortedCards.map(item => (
       <Card item={item} key={item.id} openOverlay={this.openOverlay} />
     ));
 
