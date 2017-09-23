c5194f951ff13678d4e4ee1c65785b304f6d9862
Jusna Begum <jb@juweez.co.uk>
2017-09-23 22:07:05 +0100

    Change from full screen modal to centered modal
    The full screen modal looked strange on large screens.

--- src/components/Overlay/index.js
+++ src/components/Overlay/index.js
@@ -12,22 +12,24 @@ export default function Overlay({ open, closeOverlay, item }) {
   return (
     <div className={classes}>
       <div className={styles.background}>
-        {item && (
-            <div className={styles.content}>
-              <div className={styles.imgContainer}>
-                <img className={styles.img} src={`http://juweez.co.uk/${item.photos[0]}`} alt={item.title} />
+        <div className={styles.body}>
+          {item && (
+              <div className={styles.content}>
+                <div className={styles.imgContainer}>
+                  <img className={styles.img} src={`http://juweez.co.uk/${item.photos[0]}`} alt={item.title} />
+                </div>
+                <div className={styles.details}>
+                  <h2 className={styles.title}>{item.title}</h2>
+                  <p><span className={styles['property-title']}>Brand</span> {item.brand}</p>
+                  <p><span className={styles['property-title']}>Manufacturer</span> {item.manufacturer.label || '-'}</p>
+                  <p><span className={styles['property-title']}>Release date</span> {item.date_of_issue || '-'}</p>
+                  <p><span className={styles['property-title']}>Purchase date</span> {item.date_of_purchase || '-'}</p>
+                  <p><span className={styles['property-title']}>Condition</span> {item.condition.label || '-'}</p>
+                  <p><span className={styles['property-title']}>Print run</span> {item.print_run || '-'}</p>
+                </div>
               </div>
-              <div className={styles.details}>
-                <h2 className={styles.title}>{item.title}</h2>
-                <p><span className={styles['property-title']}>Brand</span> {item.brand}</p>
-                <p><span className={styles['property-title']}>Manufacturer</span> {item.manufacturer.label || '-'}</p>
-                <p><span className={styles['property-title']}>Release date</span> {item.date_of_issue || '-'}</p>
-                <p><span className={styles['property-title']}>Purchase date</span> {item.date_of_purchase || '-'}</p>
-                <p><span className={styles['property-title']}>Condition</span> {item.condition.label || '-'}</p>
-                <p><span className={styles['property-title']}>Print run</span> {item.print_run || '-'}</p>
-              </div>
-            </div>
-        )}
+          )}
+        </div>
       </div>
       <span className={styles.close} onClick={closeOverlay}>X</span>
     </div>
