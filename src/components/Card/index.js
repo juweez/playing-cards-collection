dab2e241116369606a944f47c32a5489103d0845
Jusna Begum <jb@juweez.co.uk>
2017-09-25 14:24:50 +0100

    Lazy load card images

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LazyLoad from 'react-lazyload';
 
 import styles from './card.scss';
 
 export default function Card({ item, openOverlay }) {
   return (
     <div className={styles.container} onClick={() => openOverlay(item)}>
-      <div className={styles.imgContainer}>
+      <LazyLoad height={250} offset={150}>
         <img className={styles.img} src={`http://juweez.co.uk/${item.photos[0]}`} alt={item.title} />
-      </div>
+      </LazyLoad>
     </div>
   );
 }
